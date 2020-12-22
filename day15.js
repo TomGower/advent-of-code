087800bafaca326b653aff6cb6241b406330ef3d
TomGower <tom.gower@gmail.com>
2020-12-22 13:39:38 -0600

    Minor size optimization, speed still not great

--- day15.js
+++ day15.js
@@ -1,45 +1,34 @@
-const input = ``;
+const startingNums = [1,0,15,2,10,13];
 
-const inputArray = input.split('\n');
-
-const turnsToTake = 2020;
-const nums = [1,0,15,2,10,13];
-
-const partOne = (turns) => {
-  // const nums = [0, 3, 6]; // test case
+const playGame = (turns, part) => {
   const spoken = new Map();
 
   let prev = null;
   let last = null;
-  let prevIndex = null;
   let currentTurn = 1;
   while (currentTurn <= turns) {
-    if (currentTurn <= nums.length) {
-      prev = nums[currentTurn - 1];
+    if (currentTurn <= startingNums.length) {
+      prev = startingNums[currentTurn - 1];
       last = spoken.get(prev);
-      last ? last.push(currentTurn) : last = [currentTurn];
+      last ? last = [last[last.length - 1], currentTurn] : last = [currentTurn];
       spoken.set(prev, last);
     } else {
       last = spoken.get(prev);
       if (last.length < 2) {
         prev = 0;
-        prevIndex = spoken.get(prev);
-        prevIndex ? prevIndex.push(currentTurn) : prevIndex = [currentTurn];
-        spoken.set(prev, prevIndex);
       } else {
         prev = last[last.length - 1] - last[last.length - 2];
-        prevIndex = spoken.get(prev);
-        prevIndex ? prevIndex.push(currentTurn) : prevIndex = [currentTurn];
-        spoken.set(prev, prevIndex);
       }
+      let prevIndex = spoken.get(prev);
+      prevIndex ? prevIndex = [prevIndex[prevIndex.length - 1], currentTurn] : prevIndex = [currentTurn];
+      spoken.set(prev, prevIndex);
     }
-    // console.log(prev); // used for proof of concept on test case
     currentTurn++;
   }
 
-  console.log('part one', prev);
+  console.log('part', part, prev);
 }
 
-partOne(2020);
+playGame(2020, 'one'); // 211
 
-partOne(30000000);
+playGame(30000000, 'two'); // 2159626
