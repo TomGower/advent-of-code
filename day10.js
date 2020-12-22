79cc10d9e4cbcfab67ea361ae71a76977ea6d82f
TomGower <tom.gower@gmail.com>
2020-12-22 11:56:24 -0600

    Refactored input to separate file

--- day10.js
+++ day10.js
@@ -1,102 +1,7 @@
-const input = `77
-10
-143
-46
-79
-97
-54
-116
-60
-91
-80
-132
-20
-154
-53
-14
-103
-31
-65
-110
-43
-38
-47
-120
-112
-87
-24
-95
-33
-104
-73
-22
-66
-137
-21
-109
-118
-63
-55
-124
-146
-148
-84
-86
-147
-125
-23
-85
-117
-71
-48
-136
-151
-130
-83
-56
-140
-9
-49
-113
-131
-133
-74
-37
-127
-34
-32
-106
-1
-78
-11
-72
-40
-96
-17
-64
-92
-102
-123
-126
-90
-105
-57
-99
-27
-70
-98
-111
-30
-50
-67
-2
-155
-5
-119
-8
-39`;
-
-const inputArray = input.split('\n');
+import { readFileSync } from 'fs';
+import path from 'path';
+const __dirname = path.resolve(path.dirname(''));
+const inputArray = readFileSync(__dirname + '/inputs/day10.input', 'utf8').split('\n');
 
 let max = -Infinity;
 let voltages = new Set();
@@ -124,7 +29,7 @@ const partOne = () => {
     }
   }
 
-  console.log('part one:', oneDiffs * (threeDiffs + 1));
+  console.log('part one:', oneDiffs * (threeDiffs + 1)); // 2040
 }
 
 partOne();
@@ -141,7 +46,7 @@ const partTwo = () => {
     }
   }
 
-  console.log('part two:', dp[dp.length-1]);
+  console.log('part two:', dp[dp.length-1]); // 28346956187648
 }
 
 partTwo();
