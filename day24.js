57ef539e4aa2c07181c6966625154fbb3dc1ef8d
TomGower <tom.gower@gmail.com>
2020-12-23 23:09:08 -0600

    Completed step parser function

--- day24.js
+++ day24.js
@@ -1,5 +1,20 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
-const inputArray = readFileSync(__dirname + '/inputs/day24.input', 'utf8').split('');
+const inputArray = readFileSync(__dirname + '/inputs/day24.input', 'utf8').split('\n');
+
+const parseSteps = str => {
+  let res = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'n' || str[i] === 's') {
+      res.push(str.substring(i, i+2));
+      i++;
+    } else {
+      res.push(str[i]);
+    }
+  }
+  return res;
+}
+
+const steps = inputArray.map(parseSteps);
 
