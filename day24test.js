68e1b3da056cc6d0c7e0b3836c9d01ab1fd7737b
TomGower <tom.gower@gmail.com>
2020-12-24 01:05:20 -0600

    Mucking with pt 2, day 8 ok but 9 off

--- day24test.js
+++ day24test.js
@@ -35,6 +35,19 @@ const parseSteps = str => {
 }
 
 const moves = inputArray.map(parseSteps);
+let xMin = Infinity;
+let xMax = -Infinity;
+let yMin = Infinity;
+let yMax = -Infinity;
+
+const updateMinMax = (a, b) => {
+  xMin = Math.min(xMin, +a);
+  xMax = Math.max(xMax, +a);
+  yMin = Math.min(yMin, +b);
+  yMax = Math.max(yMax, +b);
+}
+const makeKey = (a, b) => a + '|' + b;
+
 
 let destinations = new Set();
 for (const move of moves) {
@@ -64,16 +77,72 @@ for (const move of moves) {
       throw new Error('this should be impossible');
     }
   }
-  const key = `${x}|${y}`;
-  console.log(key);
+  const key = makeKey(x, y);
+  updateMinMax(x, y);
   if (destinations.has(key)) {
-    // console.log('found duplicate', key);
     destinations.delete(key);
   }
   else (destinations.add(key));
 }
 
-// console.log(destinations);
 console.log('part one', destinations.size);
-// not 384
-// not 210
+
+const checkNeighbors = (x, y) => {
+  let total = 0;
+  total += destinations.has(makeKey(x + 2, y)) + destinations.has(makeKey(x - 2, y)) + destinations.has(makeKey(x - 1, y - 1)) +
+    destinations.has(makeKey(x + 1, y + 1)) + destinations.has(makeKey(x + 1, y - 1)) + destinations.has(makeKey(x - 1, y + 1));
+  return total;
+}
+
+console.log('3 -3 check', checkNeighbors(3, -3), destinations.has(makeKey(3, -3)));
+
+let round = 0;
+const rounds = 9;
+while (round < rounds) {
+  // do stuff
+  if (Math.abs(xMin % 2) === 1) xMin--;
+  if (Math.abs(yMin % 2) === 1) yMin--;
+  if (xMax % 2 === 1) xMax++;
+  if (yMax % 2 === 1) yMax++;
+  let nextDestinations = new Set();
+  console.log(xMin, yMin, xMax, yMax, '|', xMin-4, yMin-4);
+  for (let i = xMin - 4; i <= xMax + 2; i += 2) {
+    for (let j = yMin - 4; j <= yMax + 2; j += 2) {
+      const neighbors = checkNeighbors(i, j);
+      // console.log(i, j, neighbors);
+      const key = makeKey(i, j);
+      if (destinations.has(key)) {
+        if (neighbors === 1 || neighbors === 2) {
+          nextDestinations.add(key);
+          updateMinMax(i, j);
+        }
+      } else {
+        if (neighbors === 2) {
+          nextDestinations.add(key);
+          updateMinMax(i, j);
+        }
+      }
+      const neighbors2 = checkNeighbors(i + 1, j + 1);
+      const key2 = makeKey(i + 1, j + 1);
+      if (i + 1 === -13) console.log('-13 j', j)
+      if (i + 1 === -13) console.log(neighbors2);
+      if (destinations.has(key2)) {
+        if (neighbors2 === 1 || neighbors2 === 2) {
+          nextDestinations.add(key2);
+          updateMinMax(i + 1, j + 1);
+        }
+      } else {
+        if (neighbors2 === 2) {
+          nextDestinations.add(key2);
+          updateMinMax(i + 1, j + 1);
+        }
+      }
+    }
+  }
+  destinations = nextDestinations;
+  console.log(round, destinations.size, destinations);
+  round++;
+}
+
+console.log(destinations);
+console.log('destinations', destinations.size);
