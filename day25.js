6306efca40eee9e56c5cdbc8987ef1cd6097d5a2
TomGower <tom.gower@gmail.com>
2020-12-24 23:56:31 -0600

    Minor cleanup

--- day25.js
+++ day25.js
@@ -4,16 +4,15 @@ const __dirname = path.resolve(path.dirname(''));
 const [cardPublicKey, doorPublicKey] = readFileSync(__dirname + '/inputs/day25.input', 'utf8').split('\n').map(Number);
 
 const factor = 20201227;
-let cardLoopSize = 0;
-let cardVal = 1;
-
+const initialSubjectNumber = 7;
 const transform = (val, subjectNumber) => {
-  const interim = val * subjectNumber;
-  return interim % factor;
+  return (val * subjectNumber) % factor;
 }
 
+let cardLoopSize = 0;
+let cardVal = 1;
 while (cardVal !== cardPublicKey) {
-  cardVal = transform(cardVal, 7);
+  cardVal = transform(cardVal, initialSubjectNumber);
   cardLoopSize++;
 }
 console.log('card loop size', cardLoopSize);
@@ -21,23 +20,23 @@ console.log('card loop size', cardLoopSize);
 let doorLoopSize = 0;
 let doorVal = 1;
 while (doorVal !== doorPublicKey) {
-  doorVal = transform(doorVal, 7);
+  doorVal = transform(doorVal, initialSubjectNumber);
   doorLoopSize++;
 }
 console.log('door loop size', doorLoopSize);
-
-let doorKey = 1;
-for (let i = 0; i < doorLoopSize; i++) {
-  doorKey = transform(doorKey, cardPublicKey);
-}
 
 let cardKey = 1;
 for (let i = 0; i < cardLoopSize; i++) {
   cardKey = transform(cardKey, doorPublicKey);
 }
 
-if (doorKey === cardKey) {
-  console.log('part one: encryption key', doorKey);
+let doorKey = 1;
+for (let i = 0; i < doorLoopSize; i++) {
+  doorKey = transform(doorKey, cardPublicKey);
+}
+
+if (cardKey === doorKey) {
+  console.log('part one: encryption key', cardKey);
 } else {
   throw new Error('encryption keys do not match');
 }
