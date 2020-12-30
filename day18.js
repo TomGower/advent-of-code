780e9f24cb2e43d889ab948bdae33ffed596318c
TomGower <tom.gower@gmail.com>
2020-12-30 17:13:23 -0600

    Figured it out, got part 1

--- day18.js
+++ day18.js
@@ -39,5 +39,38 @@ Before you can help with the homework, you need to understand it yourself. Evalu
 */
 
 const partOne = () => {
-  
+    const evaluate = str => {
+        let stack = [];
+        for (let i = 0; i < str.length; i++) {
+        if (str[i] === ' ') continue;
+        else if (str[i] === '(') {
+            let opens = 1;
+            let index = i + 1;
+            while (opens > 0) {
+            if (str[index] === '(') opens++;
+            else if (str[index] === ')') opens--;
+            index++;
+            }
+            const val = evaluate(str.substring(i + 1, index - 1));
+            stack.push(val);
+            i = index;
+        }
+        else stack.push(str[i]);
+        }
+        console.log(stack);
+        let res = eval(`${stack[0]}${stack[1]}${stack[2]}`);
+        for (let i = 3; i < stack.length; i = i + 2) {
+        res = eval(`${res}${stack[i]}${stack[i+1]}`);
+        }
+        return res;
+    }
+
+    let total = 0;
+    for (const operation of operations) {
+        total += evaluate(operation);
+    }
+
+    console.log('part one', total); // 3159145843816
 }
+
+partOne();
