170a2e5a684fc4c139db530e8db2d3b993847ed2
TomGower <tom.gower@gmail.com>
2023-12-13 16:52:04 -0600

    Day 5 Part 2

--- 2023/day05test.js
+++ 2023/day05test.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
-const values = readFileSync(__dirname + '/2023/inputs/day05.input', 'utf8').split('\n\n');
+const values = readFileSync(__dirname + '/2023/inputs/day05test.input', 'utf8').split('\n\n');
 
 let target = values[0].split(':')[1].trim().split(' ').map(v => + v);
 
@@ -10,6 +10,9 @@ function createMaps(s) {
 }
 
 const maps = values.slice(1).map(createMaps);
+
+console.log('initial target', target);
+console.log('map count is', maps.length);
 
 for (const map of maps) {
   const nextTarget = [];
@@ -26,53 +29,42 @@ for (const map of maps) {
   target = nextTarget;
 }
 
-console.log('part 1', target.reduce((acc, curr) => Math.min(acc, curr), Infinity)); // NOT 73669231, IS 484023871
+console.log('part 1', target.reduce((acc, curr) => Math.min(acc, curr), Infinity), 'should be 35');
 
 let targetRanges = values[0].split(':')[1].trim().split(' ').map(v => + v);
 
+console.log('initial target ranges', targetRanges)
+
 for (const map of maps) {
   const nextTargetRanges = [];
+  console.log('targetRange', targetRanges);
   for (let i = 0; i < targetRanges.length; i += 2) {
     let min = targetRanges[i];
     let max = min + targetRanges[i + 1] - 1;
     let isAdded = false;
     for (const [dest, src, range] of map) {
-      // if (min >= src && min < src + range) {
-      //   const last = Math.min(src + range - 1, max);
-      //   nextTargetRanges.push(dest + min - src, dest + min - src + last);
-      //   if (last < max) {
-      //     console.log('adding a new range at the back')
-      //     min = last + 1;
-      //   } else {
-      //     isAdded = true;
-      //   }
-      // } else if (max >= src && max < src + range) {
-      //   console.log('adding a new range at the front')
-      //   nextTargetRanges.push(dest, src + range - max + 1)
-      //   max = src - 1;
-      // }
-
-
       const firstSrc = src;
       const lastSrc = src + range - 1;
+      console.log('firstSrc, lastSrc', firstSrc, lastSrc, min, max);
       if (min > lastSrc) continue;
       if (max < firstSrc) continue;
       // case 1: min/max fit in range
       if (min >= firstSrc && max <= lastSrc) {
-        nextTargetRanges.push(dest + min - firstSrc, max - min + 1);
+        nextTargetRanges.push(dest + min - src, targetRanges[i + 1]);
+        console.log('case 1', nextTargetRanges)
         isAdded = true;
       // case 2: max in range, min below range
       } else if (max <= lastSrc && min < firstSrc) {
-        nextTargetRanges.push(dest, max - firstSrc + 1);
+        nextTargetRanges.push(dest, max - src + 1);
         max = firstSrc - 1;
       // case 3: min in range, max above range
       } else if (min >= firstSrc && max > lastSrc) {
-        nextTargetRanges.push(dest + min - src, lastSrc - min + 1);
+        nextTargetRanges.push(dest + min - src, range - 1 - (min - src));
         min = lastSrc + 1;
       // case 4: range of dest fits within range of srcs
       } else if (min < firstSrc && max > lastSrc) {
         nextTargetRanges.push(dest, range);
-        targetRanges.push(lastSrc + 1, max - lastSrc);
+        targetRanges.push(src + range, max - (src + range) + 1);
         max = firstSrc - 1;
       } else {
         console.log('unhandled case')
@@ -89,4 +81,4 @@ for (let i = 0; i < targetRanges.length; i += 2) {
   p2Min = Math.min(p2Min, targetRanges[i]);
 }
 
-console.log('part 2', p2Min); // NOT 6434225, too low, IS 46294175
+console.log('part 2', p2Min, 'should be 46');
