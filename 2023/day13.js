8fe790bb4d4d65eaf61bece19fe974c320813f8e
TomGower <tom.gower@gmail.com>
2023-12-14 23:07:30 -0600

    Day 13 Part 2

--- 2023/day13.js
+++ 2023/day13.js
@@ -69,3 +69,57 @@ console.log('part one', p1);
 // NOT 30100, too low
 // NOT 34733, not just 1-indexing problem
 // IS 36041, problem was I was starting the arrays in findReflection at 1 and not 0 and didn't change after I fixed symmetry conditions
+
+function findHorizontalSmudge(puzzle) {
+  for (let i = 0; i < puzzle.length - 1; i++) {
+    let k = 0;
+    let cumulativeErrors = 0;
+    while ((i - k) >= 0 && (i + 1 + k) < puzzle.length) {
+      let errors = 0;
+      for (let j = 0; j < puzzle[i].length; j++) {
+        if (puzzle[i - k][j] !== puzzle[i + 1 + k][j]) {
+          errors++;
+          cumulativeErrors++;
+        }
+      }
+      if (cumulativeErrors > 1) break;
+      k++;
+    }
+    if (i - k < 0 || i + 1 + k === puzzle.length) {
+      if (cumulativeErrors === 1) return i + 1;
+    }
+  }
+  return 0;
+}
+
+function findVerticalSmudge(puzzle) {
+  for (let j = 0; j < puzzle[0].length - 1; j++) {
+    let k = 0;
+    let cumulativeErrors = 0;
+    while (((j - k) >= 0) && ((j + 1 + k) < puzzle[0].length)) {
+      for (let i = 0; i < puzzle.length; i++) {
+        if (puzzle[i][j - k] !== puzzle[i][j + 1 + k]) cumulativeErrors++;
+      }
+      if (cumulativeErrors > 1) break;
+      k++;
+    }
+    if (j - k < 0 || j + 1 + k === puzzle[0].length) {
+      if (cumulativeErrors === 1) return j + 1;
+    }
+  }
+  return 0;
+}
+
+function partTwo() {
+  const input = readFileSync(__dirname + '/2023/inputs/day13.input', 'utf8');
+  const puzzles = input.split('\n\n');
+  const puzzleArrays = puzzles.map(p => p.split('\n'));
+  const horizontal = puzzleArrays.map(findHorizontalSmudge);
+  const vertical = puzzleArrays.map(findVerticalSmudge);
+
+  return horizontal.reduce((acc, curr) => acc + curr * 100, 0) + vertical.reduce((acc, curr) => acc + curr, 0);
+}
+
+const p2 = partTwo();
+
+console.log('part two', p2); // 35915
