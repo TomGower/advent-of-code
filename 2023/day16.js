94e3e5648af864284a6a93b0e1648c5aee983351
TomGower <tom.gower@gmail.com>
2023-12-18 15:40:03 -0600

    Day 16 Part 2

--- 2023/day16.js
+++ 2023/day16.js
@@ -2,46 +2,44 @@ import { readFileSync } from 'fs';
 import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
 
-const seen = new Set();
-
-function traverse(r, c, direction, grid, marked) {
+function traverse(r, c, direction, grid, marked, seen = new Set()) {
   if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length) return;
   const key = `${r}~${c}~${direction}`;
   if (seen.has(key)) return;
   seen.add(key);
   marked[r][c] = true;
   if (grid[r][c] === '.') {
-    if (direction === 'up') traverse(r - 1, c, direction, grid, marked);
-    if (direction === 'down') traverse(r + 1, c, direction, grid, marked);
-    if (direction === 'left') traverse(r, c - 1, direction, grid, marked);
-    if (direction === 'right') traverse(r, c + 1, direction, grid, marked);
+    if (direction === 'up') traverse(r - 1, c, direction, grid, marked, seen);
+    if (direction === 'down') traverse(r + 1, c, direction, grid, marked, seen);
+    if (direction === 'left') traverse(r, c - 1, direction, grid, marked, seen);
+    if (direction === 'right') traverse(r, c + 1, direction, grid, marked, seen);
   }
   if (grid[r][c] === '|') {
-    if (direction === 'up') traverse(r - 1, c, direction, grid, marked);
-    if (direction === 'down') traverse(r + 1, c, direction, grid, marked);
+    if (direction === 'up') traverse(r - 1, c, direction, grid, marked, seen);
+    if (direction === 'down') traverse(r + 1, c, direction, grid, marked, seen);
     else {
-      traverse(r - 1, c, 'up', grid, marked);
-      traverse(r + 1, c, 'down', grid, marked);
+      traverse(r - 1, c, 'up', grid, marked, seen);
+      traverse(r + 1, c, 'down', grid, marked, seen);
     }
   }
   if (grid[r][c] === '-') {
-    if (direction === 'left') traverse(r, c - 1, direction, grid, marked);
-    if (direction === 'right') traverse(r, c + 1, direction, grid, marked); else {
-      traverse(r, c - 1, 'left', grid, marked);
-      traverse(r, c + 1, 'right', grid, marked);
+    if (direction === 'left') traverse(r, c - 1, direction, grid, marked, seen);
+    if (direction === 'right') traverse(r, c + 1, direction, grid, marked, seen); else {
+      traverse(r, c - 1, 'left', grid, marked, seen);
+      traverse(r, c + 1, 'right', grid, marked, seen);
     }
   }
   if (grid[r][c] === '/') {
-    if (direction === 'up') traverse(r, c + 1, 'right', grid, marked);
-    if (direction === 'down') traverse(r, c - 1, 'left', grid, marked);
-    if (direction === 'left') traverse(r + 1, c, 'down', grid, marked);
-    if (direction === 'right') traverse(r - 1, c, 'up', grid, marked);
+    if (direction === 'up') traverse(r, c + 1, 'right', grid, marked, seen);
+    if (direction === 'down') traverse(r, c - 1, 'left', grid, marked, seen);
+    if (direction === 'left') traverse(r + 1, c, 'down', grid, marked, seen);
+    if (direction === 'right') traverse(r - 1, c, 'up', grid, marked, seen);
   }
   if (grid[r][c] === '\\') {
-    if (direction === 'up') traverse(r, c - 1, 'left', grid, marked);
-    if (direction === 'down') traverse(r, c + 1, 'right', grid, marked);
-    if (direction === 'left') traverse(r - 1, c, 'up', grid, marked);
-    if (direction === 'right') traverse(r + 1, c, 'down', grid, marked);
+    if (direction === 'up') traverse(r, c - 1, 'left', grid, marked, seen);
+    if (direction === 'down') traverse(r, c + 1, 'right', grid, marked, seen);
+    if (direction === 'left') traverse(r - 1, c, 'up', grid, marked, seen);
+    if (direction === 'right') traverse(r + 1, c, 'down', grid, marked, seen);
   }
 }
 
@@ -57,3 +55,41 @@ function partOne() {
 const p1 = partOne();
 
 console.log('part one', p1); // 6514
+
+function partTwo() {
+  const input = readFileSync(__dirname + '/2023/inputs/day16.input', 'utf8');
+  const grid = input.split('\n');
+
+  let res = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    const marked = new Array(grid.length).fill().map(_ => new Array(grid[0].length).fill(false));
+    traverse(i, 0, 'right', grid, marked);
+    let curr = marked.reduce((acc, curr) => acc + curr.filter(v => v).length, 0);
+    res = Math.max(res, curr);
+  }
+  for (let i = 0; i < grid.length; i++) {
+    const marked = new Array(grid.length).fill().map(_ => new Array(grid[0].length).fill(false));
+    traverse(i, grid[i].length - 1, 'left', grid, marked);
+    let curr = marked.reduce((acc, curr) => acc + curr.filter(v => v).length, 0);
+    res = Math.max(res, curr);
+  }
+  for (let i = 0; i < grid[0].length; i++) {
+    const marked = new Array(grid.length).fill().map(_ => new Array(grid[0].length).fill(false));
+    traverse(0, i, 'down', grid, marked);
+    let curr = marked.reduce((acc, curr) => acc + curr.filter(v => v).length, 0);
+    res = Math.max(res, curr);
+  }
+  for (let i = 0; i < grid[0].length; i++) {
+    const marked = new Array(grid.length).fill().map(_ => new Array(grid[0].length).fill(false));
+    traverse(grid.length - 1, i, 'down', grid, marked);
+    let curr = marked.reduce((acc, curr) => acc + curr.filter(v => v).length, 0);
+    res = Math.max(res, curr);
+  }
+
+  return res;
+}
+
+const p2 = partTwo();
+
+console.log('part two', p2); // 8089
