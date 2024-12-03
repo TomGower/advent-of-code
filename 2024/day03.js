a6b862ee80acd9247e8affe8f52c4d72333f17e4
TomGower <tom.gower@gmail.com>
2024-12-03 13:32:16 -0600

    2024 Day 3 Part 2

--- 2024/day03.js
+++ 2024/day03.js
@@ -5,24 +5,56 @@ const input = readFileSync(__dirname + '/inputs/day03.input', 'utf8').split(
   '\n'
 );
 
+const test =
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
 function partOne() {
-  let res = 0;
-
-  for (const row of input) {
-    const exp = /mul\(\d{1,3},\d{1,3}\)/g;
-    const matches = row.match(exp);
-    for (const match of matches) {
-      const numExp = /(\d{1,3}),(\d{1,3})/;
-      const numMatch = match.match(numExp);
-      res += +numMatch[1] * +numMatch[2];
-    }
-  }
-
-  return res;
+  return input.map(parse).reduce((acc, curr) => acc + curr, 0);
 }
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function getValue(matches) {
+  if (!matches) return 0;
+  let res = 0;
+  for (const match of matches) {
+    const numExp = /(\d{1,3}),(\d{1,3})/;
+    const numMatch = match.match(numExp);
+    res += +numMatch[1] * +numMatch[2];
+  }
+  return res;
+}
+
+function parse(s) {
+  const exp = /mul\(\d{1,3},\d{1,3}\)/g;
+  if (typeof s !== 'string')
+    throw new Error(`${s} is not a string, s is a ${typeof s}`);
+  const matches = s.match(exp);
+  return getValue(matches);
+}
+
+function partTwo() {
+  let res = 0;
+
+  // const modifiedInput = readFileSync(
+  //   __dirname + '/inputs/day03a.input',
+  //   'utf8'
+  // ).split('\n');
+  // return modifiedInput.map(parse).reduce((acc, curr) => acc + curr, 0);
+
+  const joinedInput = input.join('');
+  // const joinedInput = test;
+
+  const rows = joinedInput.split("don't()");
+  res += parse(rows[0]);
+  const modifiedRows = rows.slice(1);
+
+  for (const row of modifiedRows) {
+    const activeRows = row.split('do()').slice(1).join('');
+    res += parse(activeRows);
+  }
+
+  return res;
+}
 
 console.log('The answer to Part Two may be', partTwo());
