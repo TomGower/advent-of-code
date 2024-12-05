26b090932fc6b432f695f6a39d974079ddd293f2
TomGower <tom.gower@gmail.com>
2024-12-05 11:42:35 -0600

    2024 Day 5 Part 2

--- 2024/day05.js
+++ 2024/day05.js
@@ -46,6 +46,70 @@ function partOne() {
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function makeValid(update, outMap) {
+  const values = new Set(update);
+
+  const map = new Map();
+  for (const v of values) {
+    map.set(v, 0);
+  }
+
+  const updateOutMap = new Map();
+
+  for (const v of values) {
+    const outdegree = outMap.get(v) ?? new Set();
+    const updateOuts = outdegree.intersection(values);
+    updateOutMap.set(v, updateOuts);
+    for (const v of updateOuts) {
+      map.set(v, map.get(v) + 1);
+    }
+  }
+
+  const res = [];
+
+  let current;
+
+  for (const [k, v] of map) {
+    if (v === 0) {
+      current = k;
+      break;
+    }
+  }
+
+  while (res.length < update.length) {
+    res.push(current);
+    const outs = updateOutMap.get(current) ?? new Set();
+    let next;
+    for (const v of outs) {
+      map.set(v, map.get(v) - 1);
+      if (map.get(v) === 0) next = v;
+    }
+    current = next;
+  }
+
+  return res;
+}
+
+function partTwo() {
+  const inMap = new Map();
+  const outMap = new Map();
+  for (const [before, after] of orderingRules) {
+    if (!outMap.has(before)) outMap.set(before, new Set());
+    outMap.get(before).add(after);
+    if (!inMap.has(after)) inMap.set(after, new Set());
+    inMap.get(after).add(before);
+  }
+
+  const invalidUpdates = updates.filter((v) => !isValid(v, inMap));
+
+  let res = 0;
+
+  for (const update of invalidUpdates) {
+    const validUpdate = makeValid(update, outMap);
+    res += validUpdate[Math.floor(validUpdate.length / 2)];
+  }
+
+  return res;
+}
 
 console.log('The answer to Part Two may be', partTwo());
