841046b507b9866d8773e3fc4e1e1dd9ebed2b67
TomGower <tom.gower@gmail.com>
2024-12-09 14:10:48 -0600

    2024 Day 9 Part 2

--- 2024/day09.js
+++ 2024/day09.js
@@ -3,7 +3,6 @@ import { readFileSync } from 'fs';
 import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
 const input = readFileSync(__dirname + '/inputs/day09.input', 'utf8');
-// const input = '12345';
 
 function compareFilled(a, b) {
   return b[1] - a[1];
@@ -20,7 +19,6 @@ function partOne() {
   const openStack = new PriorityQueue(compareOpen);
   for (let i = 0; i < input.length; i++) {
     const len = Number(input[i]);
-    // console.log('len is', len, startIdx, counter);
     if (i % 2 === 0) {
       filledStack.enqueue([len, startIdx, counter]);
       counter++;
@@ -53,6 +51,61 @@ function partOne() {
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function binInsert(files, file) {
+  const targetIdx = file[1];
+  for (let i = 0; i < files.length; i++) {
+    const fileIdx = files[i][1];
+    if (targetIdx < fileIdx) {
+      files.splice(i, 0, file);
+      return;
+    }
+  }
+  files.push(file);
+}
+
+function getFileSum([len, startIdx, counter]) {
+  let res = 0;
+  for (let i = 0; i < len; i++) {
+    res += (startIdx + i) * counter;
+  }
+  return res;
+}
+
+function partTwo() {
+  let files = [];
+  const opens = new PriorityQueue(compareOpen);
+  let startIdx = 0;
+  let counter = 0;
+  for (let i = 0; i < input.length; i++) {
+    const len = Number(input[i]);
+    if (i % 2 === 0) {
+      files.push([len, startIdx, counter]);
+      counter++;
+    } else {
+      opens.enqueue([len, startIdx]);
+    }
+    startIdx += len;
+  }
+  while (!opens.isEmpty()) {
+    const [len, startIdx] = opens.dequeue();
+    let availableFileIdx = -1;
+    for (let i = files.length - 1; i >= 0; i--) {
+      if (files[i][1] < startIdx) break;
+      if (files[i][0] <= len) {
+        availableFileIdx = i;
+        break;
+      }
+    }
+    if (availableFileIdx === -1) continue;
+    const availableFile = files[availableFileIdx];
+    availableFile[1] = startIdx;
+    files.splice(availableFileIdx, 1);
+    binInsert(files, availableFile);
+    if (len > availableFile[0]) {
+      opens.enqueue([len - availableFile[0], startIdx + availableFile[0]]);
+    }
+  }
+  return files.reduce((acc, curr) => acc + getFileSum(curr), 0);
+}
 
 console.log('The answer to Part Two may be', partTwo());
