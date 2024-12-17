cdf3dc1db0896f5c0b7de7b1a2c3900be13c13b3
TomGower <tom.gower@gmail.com>
2024-12-16 18:14:31 -0600

    Day 15 Part 2

--- 2024/day15.js
+++ 2024/day15.js
@@ -18,8 +18,6 @@ function findStartPosition(grid, target) {
   throw new Error(`target ${target} could not be found in grid`);
 }
 
-const [startX, startY] = findStartPosition(grid, '@');
-
 function canMove(r, c, direction, grid) {
   const [dr, dc] = direction;
   (r = r + dr), (c = c + dc);
@@ -48,8 +46,9 @@ function move(r, c, [dr, dc], grid) {
 }
 
 function partOne() {
+  const [startX, startY] = findStartPosition(grid, '@');
   let [x, y] = [startX, startY];
-  grid[startX][startY] = '.';
+  grid[x][y] = '.';
 
   for (const row of moves) {
     for (const cell of row) {
@@ -67,12 +66,148 @@ function partOne() {
     }
   }
 
+  grid[startX][startY] = '@';
+
   return res;
 }
 
 console.log('The answer to Part One may be', partOne());
 // correct at 27:30
 
-function partTwo() {}
+function embiggenGrid(grid) {
+  const res = [];
+  for (const row of grid) {
+    const newRow = [];
+    for (const cell of row) {
+      if (cell === '#') newRow.push('#', '#');
+      if (cell === 'O') newRow.push('[', ']');
+      if (cell === '.') newRow.push('.', '.');
+      if (cell === '@') newRow.push('@', '.');
+    }
+    res.push(newRow);
+  }
+  return res;
+}
+
+function canMoveBigger(r, c, direction, grid) {
+  const [dr, dc] = direction;
+  const queue = [[r + dr, c + dc]];
+  const seen = new Set();
+  while (queue.length > 0) {
+    const [nr, nc] = queue.shift();
+    const key = `${nr}~${nc}`;
+    if (seen.has(key)) continue;
+    seen.add(key);
+    if (grid[nr][nc] === '.') continue;
+    else if (grid[nr][nc] === '#') return false;
+    else {
+      if (dr === 0) queue.push([nr + dr, nc + dc]);
+      else {
+        queue.push([nr + dr, nc + dc]);
+        if (grid[nr][nc] === '[') {
+          queue.push([nr + dr, nc + 1 + dc]);
+        } else if (grid[nr][nc] === ']') {
+          queue.push([nr + dr, nc - 1 + dc]);
+        }
+      }
+    }
+  }
+  return true;
+}
+
+function moveBigger(r, c, [dr, dc], grid) {
+  if (!canMoveBigger(r, c, [dr, dc], grid)) return [r, c];
+  const [nextR, nextC] = [r + dr, c + dc];
+  if (grid[nextR][nextC] === '.') return [nextR, nextC];
+  if (grid[nextR][nextC] === '[' || grid[nextR][nextC] === ']') {
+    if (dr === 0) {
+      // pushing boxes left/right
+      while (grid[r + dr][c + dc] !== '.') {
+        if (grid[r + dr][c + dc] === '[') {
+          grid[r + dr][c + dc] = ']';
+        } else {
+          grid[r + dr][c + dc] = '[';
+        }
+        (r += dr), (c += dc);
+      }
+      grid[r + dr][c + dc] = dc > 0 ? ']' : '[';
+    } else {
+      // pushing boxes up or down
+      const queue = [];
+      queue.push([nextR, nextC, '.']);
+      const visited = new Set();
+      while (queue.length) {
+        const [row, col, val] = queue.shift();
+        const key = `${row}~${col}`;
+        if (visited.has(key)) continue;
+        visited.add(key);
+        // if this is another box, we need to move the box
+        if (grid[row][col] === '[') {
+          queue.push([row + dr, col + dc, grid[row][col]]);
+          if (!visited.has(`${row}~${col + 1}`)) {
+            queue.push([row + dr, col + 1 + dc, grid[row][col + 1]]);
+            grid[row][col + 1] = '.';
+          }
+        } else if (grid[row][col] === ']') {
+          queue.push([row + dr, col + dc, grid[row][col]]);
+          if (!visited.has(`${row}~${col - 1}`)) {
+            queue.push([row + dr, col - 1 + dc, grid[row][col - 1]]);
+            grid[row][col - 1] = '.';
+          }
+        }
+        // in all cases, update value
+        grid[row][col] = val;
+
+        // old code
+        // if (grid[row][col] === '[') {
+        //   queue.push([row + dr, col + dc, grid[row][col]]);
+        //   if (!visited.has(`${row}~${col + 1}`)) {
+        //     queue.push([row + dr, col + 1 + dc, ']']);
+        //     visited.add(`${row}~${col + 1}`);
+        //     grid[row][col + 1] = '.';
+        //   }
+        // } else if (grid[row][col] === ']') {
+        //   queue.push([row + dr, col + dr, grid[row][col]]);
+        //   if (!visited.has(`${row}~${col - 1}`)) {
+        //     queue.push([row + dr, col - 1 + dc, '[']);
+        //     visited.add(`${row}~${col - 1}`);
+        //     grid[row][col - 1] = '.';
+        //   }
+        // }
+        // grid[row][col] = val;
+      }
+    }
+    grid[nextR][nextC] = '.';
+  }
+  return [nextR, nextC];
+}
+
+function partTwo() {
+  const biggerGrid = embiggenGrid(bigGrid.split('\n').map((r) => r.split('')));
+  const [startX, startY] = findStartPosition(biggerGrid, '@');
+  let [x, y] = [startX, startY];
+  biggerGrid[x][y] = '.';
+
+  for (const row of moves) {
+    for (const cell of row) {
+      if (cell === '^') [x, y] = moveBigger(x, y, [-1, 0], biggerGrid);
+      if (cell === '>') [x, y] = moveBigger(x, y, [0, 1], biggerGrid);
+      if (cell === 'v') [x, y] = moveBigger(x, y, [1, 0], biggerGrid);
+      if (cell === '<') [x, y] = moveBigger(x, y, [0, -1], biggerGrid);
+    }
+  }
+
+  let res = 0;
+  for (let i = 0; i < biggerGrid.length; i++) {
+    for (let j = 0; j < biggerGrid[0].length; j++) {
+      if (biggerGrid[i][j] === '[') res += 100 * i + j;
+    }
+  }
+
+  biggerGrid[startX][startY] = '@';
+
+  return res;
+}
 
 console.log('The answer to Part Two may be', partTwo());
+// correct at 2h2m
