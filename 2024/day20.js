9af3294bfcf856cbcb06df6e4c881fb6f0c6a69a
TomGower <tom.gower@gmail.com>
2024-12-20 16:11:16 -0600

    2024 Day 20 Part 2

--- 2024/day20.js
+++ 2024/day20.js
@@ -1,4 +1,3 @@
-import { PriorityQueue } from '@datastructures-js/priority-queue';
 import { readFileSync } from 'fs';
 import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
@@ -55,80 +54,6 @@ function findShortestDistance(grid, start, end) {
   throw new Error(`path from ${start} to ${end} could not be found`);
 }
 
-function findShortestDistanceWithOneSlip(grid, start, end, maxTime) {
-  const queue = [[start[0], start[1], false]];
-  let count = 0;
-  let steps = 0;
-  while (steps <= maxTime) {
-    const len = queue.length;
-    for (let i = 0; i < len; i++) {
-      const [row, col, used] = queue.shift();
-      if (row === end[0] && col === end[1]) {
-        count++;
-        continue;
-      }
-      for (const [dr, dc] of dirs) {
-        const [nr, nc] = [row + dr, col + dc];
-        // const key = `${nr}~${nc}`;
-        if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length)
-          continue;
-        if (grid[nr][nc] === '#') {
-          if (!used) queue.push([nr, nc, true]);
-        }
-        // seen.add(key);
-        queue.push([nr, nc, used]);
-      }
-    }
-    steps++;
-  }
-  return count;
-}
-
-function oneSlipShortest(grid, start, end, maxTime) {
-  const minHeap = new PriorityQueue((a, b) => a.steps - b.steps);
-  const [startRow, startCol] = start;
-  const [endRow, endCol] = end;
-  const seen = new Set();
-  seen.add(`${startRow}~${startCol}`);
-  minHeap.enqueue({
-    row: startRow,
-    col: startCol,
-    used: false,
-    steps: 0,
-  });
-  let count = 0;
-  while (!minHeap.isEmpty()) {
-    const { row, col, used, steps } = minHeap.dequeue();
-    // console.log(row, col, used, steps);
-    if (steps > maxTime) break;
-    if (row === endRow && col === endCol) {
-      count++;
-      continue;
-    }
-    // console.log(Math.abs(row - endRow), Math.abs(col - endCol), steps, maxTime);
-    if (Math.abs(row - endRow) + Math.abs(col - endCol) + steps > maxTime)
-      continue;
-    for (const [dr, dc] of dirs) {
-      const [nr, nc] = [row + dr, col + dc];
-      if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length)
-        continue;
-      if (grid[nr][nc] === '#') {
-        if (!used) {
-          minHeap.enqueue({
-            row: nr,
-            col: nc,
-            used: true,
-            steps: steps + 1,
-          });
-        } else {
-          minHeap.enqueue({ row: nr, col: nc, used, steps: steps + 1 });
-        }
-      }
-    }
-  }
-  return count;
-}
-
 function fillDistances(grid, end, memo) {
   const queue = [end];
   let steps = 0;
@@ -157,7 +82,6 @@ function findShortestPathsWithOneMove(grid, start, memo, maxSteps) {
   const seen = new Set();
   seen.add(`${startRow}~${startCol}`);
   let steps = 0;
-  let count = 0;
   while (queue.length) {
     const len = queue.length;
     for (let i = 0; i < len; i++) {
@@ -184,12 +108,8 @@ function findShortestPathsWithOneMove(grid, start, memo, maxSteps) {
     steps++;
     if (steps > maxSteps) break;
   }
-  // console.log(wallQueue);
   const walls = new Set();
   for (const [row, col, stepCount] of wallQueue) {
-    const wallKey = `${row}~${col}`;
-    // if (walls.has(wallKey)) continue;
-    // walls.add(wallKey);
     for (const [dr, dc] of dirs) {
       const [nr, nc] = [row + dr, col + dc];
       if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length)
@@ -199,43 +119,16 @@ function findShortestPathsWithOneMove(grid, start, memo, maxSteps) {
       }
     }
   }
-  // console.log(walls);
   return walls.size;
 }
 
 function partOne() {
   const minDistance = findShortestDistance(mat, startPosition, endPosition);
   console.log('min distance is', minDistance);
-  // let count = 0;
-  // for (let i = 1; i < mat.length; i++) {
-  //   for (let j = 1; j < mat[0].length; j++) {
-  //     if (mat[i][j] !== '#') continue;
-  //     mat[i][j] = '.';
-  //     const newSteps = findShortestDistance(
-  //       mat,
-  //       startPosition,
-  //       endPosition,
-  //       minDistance - 100
-  //     );
-  //     if (newSteps !== -1) count++;
-  //   }
-  // }
-
-  // return count;
-
-  // return findShortestDistanceWithOneSlip(
-  //   mat,
-  //   startPosition,
-  //   endPosition,
-  //   minDistance - 100
-  // );
-  // return oneSlipShortest(mat, startPosition, endPosition, minDistance - 50);
   const memo = new Array(mat.length)
     .fill()
     .map((_) => new Array(mat[0].length).fill(Infinity));
   fillDistances(mat, endPosition, memo);
-
-  // console.table(memo);
 
   return findShortestPathsWithOneMove(
     mat,
@@ -247,6 +140,40 @@ function partOne() {
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function countPositionsUsingCheats(grid, startMemo, endMemo, maxSteps) {
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === '#') continue;
+      for (let r = -20; r <= 20; r++) {
+        for (let c = -20; c <= 20; c++) {
+          const dist = Math.abs(r) + Math.abs(c);
+          if (dist > 20) continue;
+          const [nr, nc] = [i + r, j + c];
+          if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length)
+            continue;
+          if (startMemo[i][j] + endMemo[nr][nc] + dist <= maxSteps) count++;
+        }
+      }
+    }
+  }
+  return count;
+}
+
+function partTwo() {
+  const minDistance = findShortestDistance(mat, startPosition, endPosition);
+
+  const startMemo = new Array(mat.length)
+    .fill()
+    .map((_) => new Array(mat[0].length).fill(Infinity));
+  fillDistances(mat, startPosition, startMemo);
+
+  const endMemo = new Array(mat.length)
+    .fill()
+    .map((_) => new Array(mat[0].length).fill(Infinity));
+  fillDistances(mat, endPosition, endMemo);
+
+  return countPositionsUsingCheats(mat, startMemo, endMemo, minDistance - 100);
+}
 
 console.log('The answer to Part Two may be', partTwo());
