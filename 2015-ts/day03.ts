208458e30c533446ac839685bc57b0b9a33234db
TomGower <tom.gower@gmail.com>
2025-01-08 16:02:05 -0600

    2025 Day 3 Part 2

--- 2015-ts/day03.ts
+++ 2015-ts/day03.ts
@@ -3,13 +3,13 @@ import path from 'path';
 const __dirname = path.resolve(path.dirname(''));
 const input = readFileSync(__dirname + '/inputs/day03.input', 'utf8');
 
-function partOne() {
-  const visited = new Set();
+function findHouses(directions: string): Set<string> {
+  const visited: Set<string> = new Set();
 
   let [row, col] = [0, 0];
   visited.add(`${row}~${col}`);
 
-  for (const d of input) {
+  for (const d of directions) {
     if (d === '^') row--;
     if (d === 'v') row++;
     if (d === '<') col--;
@@ -17,11 +17,27 @@ function partOne() {
     visited.add(`${row}~${col}`);
   }
 
-  return visited.size;
+  return visited;
+}
+
+function partOne() {
+  return findHouses(input).size;
 }
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function partTwo() {
+  const santaDirections = input
+    .split('')
+    .filter((_v, i) => i % 2 === 0)
+    .join('');
+  const roboSantaDirections = input
+    .split('')
+    .filter((_v, i) => i % 2 === 1)
+    .join('');
+
+  return findHouses(santaDirections).union(findHouses(roboSantaDirections))
+    .size;
+}
 
 console.log('The answer to Part Two may be', partTwo());
