dcebb43e49bc39cfaf2d94aab656402ba3afc553
TomGower <tom.gower@gmail.com>
2025-01-08 16:39:48 -0600

    2015 Day 5 Part 2

--- 2015-ts/day05.ts
+++ 2015-ts/day05.ts
@@ -25,6 +25,27 @@ function partOne() {
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function isBetterNice(s: string): boolean {
+  let hasTwoAway = false;
+  for (let i = 0; i < s.length - 2; i++) {
+    if (s[i] === s[i + 2]) {
+      hasTwoAway = true;
+      break;
+    }
+  }
+  if (!hasTwoAway) return false;
+  for (let i = 0; i < s.length - 3; i++) {
+    const pair = s.slice(i, i + 2);
+    for (let j = i + 2; j < s.length; j++) {
+      const nextPair = s.slice(j, j + 2);
+      if (pair === nextPair) return true;
+    }
+  }
+  return false;
+}
+
+function partTwo() {
+  return strings.filter(isBetterNice).length;
+}
 
 console.log('The answer to Part Two may be', partTwo());
