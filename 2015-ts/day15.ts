44d09dc60573e513fea5775385add42e7108184c
TomGower <tom.gower@gmail.com>
2025-01-13 17:12:09 -0600

    2025 Day 15 Part 2

--- 2015-ts/day15.ts
+++ 2015-ts/day15.ts
@@ -80,6 +80,49 @@ function partOne() {
 
 console.log('The answer to Part One may be', partOne());
 
-function partTwo() {}
+function findBestCookie(
+  ingredients: Ingredient[],
+  idx: number,
+  capacity: number,
+  durability: number,
+  flavor: number,
+  texture: number,
+  calories: number,
+  remaining: number
+): number {
+  if (calories > 500) return 0;
+  if (idx === ingredients.length || remaining === 0) {
+    if (remaining > 0 || calories !== 500) return 0;
+    if (
+      capacity < 0 ||
+      durability < 0 ||
+      flavor < 0 ||
+      texture < 0 ||
+      calories < 0
+    )
+      return 0;
+    return capacity * durability * flavor * texture;
+  }
+  const curr = ingredients[idx];
+  let max = 0;
+  for (let i = 1; i <= remaining; i++) {
+    const currMix = findBestCookie(
+      ingredients,
+      idx + 1,
+      capacity + i * curr.capacity,
+      durability + i * curr.durability,
+      flavor + i * curr.flavor,
+      texture + i * curr.texture,
+      calories + i * curr.calories,
+      remaining - i
+    );
+    max = Math.max(max, currMix);
+  }
+  return max;
+}
+
+function partTwo() {
+  return findBestCookie(ingredients, 0, 0, 0, 0, 0, 0, 100);
+}
 
 console.log('The answer to Part Two may be', partTwo());
