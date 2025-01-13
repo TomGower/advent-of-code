f6653d7c5933c71f530ac7dd9b6e110561ff98b0
TomGower <tom.gower@gmail.com>
2025-01-13 17:08:28 -0600

    2015 Day 15 Part 1

---
+++ 2015-ts/day15.ts
@@ -0,0 +1,85 @@
+import { readFileSync } from 'fs';
+import path, { resolve } from 'path';
+const __dirname = path.resolve(path.dirname(''));
+const input = readFileSync(__dirname + '/inputs/day15.input', 'utf8');
+
+type Ingredient = {
+  name: string;
+  capacity: number;
+  durability: number;
+  flavor: number;
+  texture: number;
+  calories: number;
+};
+
+function parseInput(input: string): any {
+  const ingredients: Ingredient[] = [];
+  const rows = input.split('\n');
+  for (const row of rows) {
+    const exp =
+      /(?<name>[A-Za-z]+): capacity (?<capacity>-?\d+), durability (?<durability>-?\d+), flavor (?<flavor>-?\d+), texture (?<texture>-?\d+), calories (?<calories>-?\d+)/;
+    const match = row.match(exp);
+
+    if (match && match.groups) {
+      ingredients.push(
+        Object.entries(match.groups).reduce((acc, [key, value]) => {
+          acc[key] = key === 'name' ? value : Number(value);
+          return acc;
+        }, {}) as Ingredient
+      );
+    }
+  }
+  return ingredients;
+}
+
+const ingredients = parseInput(input);
+
+function findOptimalPairing(
+  ingredients: Ingredient[],
+  idx: number,
+  capacity: number,
+  durability: number,
+  flavor: number,
+  texture: number,
+  calories: number,
+  remaining: number
+): number {
+  if (idx === ingredients.length || remaining === 0) {
+    if (remaining > 0) return 0;
+    if (
+      capacity < 0 ||
+      durability < 0 ||
+      flavor < 0 ||
+      texture < 0 ||
+      calories < 0
+    )
+      return 0;
+    return capacity * durability * flavor * texture;
+  }
+  const curr = ingredients[idx];
+  let max = 0;
+  for (let i = 1; i <= remaining; i++) {
+    const currMix = findOptimalPairing(
+      ingredients,
+      idx + 1,
+      capacity + i * curr.capacity,
+      durability + i * curr.durability,
+      flavor + i * curr.flavor,
+      texture + i * curr.texture,
+      calories + i * curr.calories,
+      remaining - i
+    );
+    max = Math.max(max, currMix);
+  }
+  return max;
+}
+
+function partOne() {
+  return findOptimalPairing(ingredients, 0, 0, 0, 0, 0, 0, 100);
+}
+
+console.log('The answer to Part One may be', partOne());
+
+function partTwo() {}
+
+console.log('The answer to Part Two may be', partTwo());
