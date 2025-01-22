8f2d01f27eeb672e254fcc298cbc211f2425452b
TomGower <tom.gower@gmail.com>
2025-01-22 16:33:22 -0600

    2015 Day 24 Part 2

--- 2015-ts/day24.ts
+++ 2015-ts/day24.ts
@@ -9,29 +9,39 @@ for (let i = weights.length - 2; i >= 0; i--) {
   maxSuffixWeight[i] = weights[i] + maxSuffixWeight[i + 1];
 }
 
+function generateCombinations(
+  target: number,
+  maxSize: number,
+  combinations: number[][],
+  idx: number = 0,
+  nums: number[] = []
+): void {
+  if (nums.length > maxSize) return;
+  if (target === 0) {
+    combinations.push([...nums]);
+    return;
+  }
+  for (let i = idx; i < weights.length; i++) {
+    if (weights[i] > target) continue;
+    if (maxSuffixWeight[i] < target) continue;
+    nums.push(weights[i]);
+    generateCombinations(
+      target - weights[i],
+      maxSize,
+      combinations,
+      i + 1,
+      nums
+    );
+    nums.pop();
+  }
+}
+
 function partOne() {
   const possibleCombinations: number[][] = [];
   const groupWeight = totalWeight / 3;
+  // manually calculate minimum possible group size off groupWeight
 
-  function generateCombinations(
-    target: number,
-    idx: number = 0,
-    nums: number[] = []
-  ): void {
-    if (nums.length > 6) return;
-    if (target === 0) {
-      possibleCombinations.push([...nums]);
-      return;
-    }
-    for (let i = idx; i < weights.length; i++) {
-      if (weights[i] > target) continue;
-      if (maxSuffixWeight[i] < target) continue;
-      nums.push(weights[i]);
-      generateCombinations(target - weights[i], i + 1, nums);
-      nums.pop();
-    }
-  }
-  generateCombinations(groupWeight);
+  generateCombinations(groupWeight, 6, possibleCombinations);
 
   // this totally didn't have to work, but it did
   let minProduct = Infinity;
@@ -47,7 +57,22 @@ console.time('one');
 console.log('The answer to Part One may be', partOne());
 console.timeEnd('one');
 
-function partTwo() {}
+function partTwo() {
+  const possibleCombinations: number[][] = [];
+  const groupWeight = totalWeight / 4;
+  // manually calculate minimum possible group size off groupWeight
+
+  generateCombinations(groupWeight, 5, possibleCombinations);
+
+  // again, this totally didn't have to work, but it did
+  let minProduct = Infinity;
+  for (const combo of possibleCombinations) {
+    const product = combo.reduce((acc, curr) => acc * curr, 1);
+    minProduct = Math.min(minProduct, product);
+  }
+
+  return minProduct;
+}
 
 console.time('two');
 console.log('The answer to Part Two may be', partTwo());
