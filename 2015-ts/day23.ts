05889e4c6d7c0928387318c5c5f37532434333de
TomGower <tom.gower@gmail.com>
2025-01-22 15:40:36 -0600

    2015 Day 23 Part 2

--- 2015-ts/day23.ts
+++ 2015-ts/day23.ts
@@ -4,41 +4,33 @@ const __dirname = path.resolve(path.dirname(''));
 const input = readFileSync(__dirname + '/inputs/day23.input', 'utf8');
 const instructions = input.split('\n');
 
-function partOne() {
-  const registers = {
-    a: 0,
-    b: 0,
-  };
+function operate(registers: Record<'a' | 'b', number>): number {
   let i = 0;
   while (i < instructions.length) {
     const orders = instructions[i].split(' ');
     const order = orders[0];
+    const register = orders[1][0] as 'a' | 'b';
     if (order === 'jmp') {
       i += parseInt(orders[1]);
     } else if (order === 'jie') {
-      const register = orders[1][0] as 'a' | 'b';
       if (registers[register] % 2 === 0) {
         i += parseInt(orders[2]);
       } else {
         i++;
       }
     } else if (order === 'jio') {
-      const register = orders[1][0] as 'a' | 'b';
       if (registers[register] === 1) {
         i += parseInt(orders[2]);
       } else {
         i++;
       }
     } else if (order === 'inc') {
-      const register = orders[1][0] as 'a' | 'b';
       registers[register]++;
       i++;
     } else if (order === 'tpl') {
-      const register = orders[1][0] as 'a' | 'b';
       registers[register] *= 3;
       i++;
     } else if (order === 'hlf') {
-      const register = orders[1][0] as 'a' | 'b';
       registers[register] /= 2;
       i++;
     } else {
@@ -48,11 +40,25 @@ function partOne() {
   return registers.b;
 }
 
+function partOne() {
+  const registers = {
+    a: 0,
+    b: 0,
+  };
+  return operate(registers);
+}
+
 console.time('one');
 console.log('The answer to Part One may be', partOne());
 console.timeEnd('one');
 
-function partTwo() {}
+function partTwo() {
+  const registers = {
+    a: 1,
+    b: 0,
+  };
+  return operate(registers);
+}
 
 console.time('two');
 console.log('The answer to Part Two may be', partTwo());
