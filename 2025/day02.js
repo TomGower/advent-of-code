1dc274307c446a3471997987e0304d0c80e6946e
TomGower <tom.gower@gmail.com>
2025-12-02 11:44:07 -0600

    2025 Day 2 Part 2

--- 2025/day02.js
+++ 2025/day02.js
@@ -19,3 +19,35 @@ function partOne() {
 }
 
 console.log('the answer to Part One may be', partOne());
+
+function hasRepeats(s) {
+  const len = s.length;
+  if (len <= 1) return false;
+  for (let i = Math.floor(len / 2); i >= 1; i--) {
+    if (len % i !== 0) continue;
+    const prefix = s.slice(0, i);
+    for (let j = i; j < len; j += i) {
+      if (s.slice(j, j + i) !== prefix) break;
+      if (j + i === len) return true;
+    }
+  }
+  return false;
+}
+
+function partTwo() {
+  const ranges = values.split(',').map((v) => v.split('-'));
+  let sum = 0;
+  for (const [a, b] of ranges) {
+    const numA = parseInt(a),
+      numB = parseInt(b);
+    for (let i = numA; i <= numB; i++) {
+      let isRepeated = false;
+      const strI = '' + i;
+      if (hasRepeats(strI)) sum += i;
+      if (isRepeated) sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log('the answer to Part One may be', partTwo());
