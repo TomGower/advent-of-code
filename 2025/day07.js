1e5db16b317961eb1b9784c7b8329f8d67ad039a
TomGower <tom.gower@gmail.com>
2025-12-09 11:18:29 -0600

    2025 Day 7 Part 2

--- 2025/day07.js
+++ 2025/day07.js
@@ -6,28 +6,80 @@ const grid = input.split('\n');
 const rows = grid.length;
 const cols = grid[0].length;
 
-function partOne() {
+function calculatePathsAndSplits() {
   const startCol = grid[0].indexOf('S');
   if (startCol === -1) throw new Error('cannot locate starting position');
-  let res = 0;
-  let beams = new Array(cols).fill(false);
-  beams[startCol] = true;
-  for (let row = 1; row < rows; row++) {
-    const nextBeams = new Array(cols).fill(false);
+  let beams = new Array(cols).fill(0);
+  let splits = 0;
+  beams[startCol] = 1;
+  for (let row = 2; row < rows; row += 2) {
+    const nextBeams = new Array(cols).fill(0);
     for (let col = 0; col < cols; col++) {
       if (beams[col]) {
         if (grid[row][col] === '^') {
-          if (col > 0) nextBeams[col - 1] = true;
-          if (col < cols - 1) nextBeams[col + 1] = true;
-          res++;
+          splits++;
+          nextBeams[col - 1] += beams[col];
+          nextBeams[col + 1] += beams[col];
         } else {
-          nextBeams[col] = true;
+          nextBeams[col] += beams[col];
         }
       }
     }
     beams = nextBeams;
+    // console.log(beams);
   }
-  return res;
+  const total = beams.reduce((acc, curr) => acc + curr, 0);
+  return [splits, total];
 }
 
-console.log('the answer to Part One may be', partOne());
+function partOne() {
+  // const startCol = grid[0].indexOf('S');
+  // if (startCol === -1) throw new Error('cannot locate starting position');
+  // let res = 0;
+  // let beams = new Array(cols).fill(false);
+  // beams[startCol] = true;
+  // for (let row = 2; row < rows; row += 2) {
+  //   const nextBeams = new Array(cols).fill(false);
+  //   for (let col = 0; col < cols; col++) {
+  //     if (beams[col]) {
+  //       if (grid[row][col] === '^') {
+  //         nextBeams[col - 1] = true;
+  //         nextBeams[col + 1] = true;
+  //         res++;
+  //       } else {
+  //         nextBeams[col] = true;
+  //       }
+  //     }
+  //   }
+  //   beams = nextBeams;
+  // }
+  // return res;
+}
+
+const [splits, total] = calculatePathsAndSplits();
+
+console.log('the answer to Part One may be', splits);
+
+function partTwo() {
+  // const startCol = grid[0].indexOf('S');
+  // if (startCol === -1) throw new Error('cannot locate starting position');
+  // let beams = new Array(cols).fill(0);
+  // beams[startCol] = 1;
+  // for (let row = 2; row < rows; row += 2) {
+  //   const nextBeams = new Array(cols).fill(0);
+  //   for (let col = 0; col < cols; col++) {
+  //     if (beams[col]) {
+  //       if (grid[row][col] === '^') {
+  //         nextBeams[col - 1] += beams[col];
+  //         nextBeams[col + 1] += beams[col];
+  //       } else {
+  //         nextBeams[col] += beams[col];
+  //       }
+  //     }
+  //   }
+  //   beams = nextBeams;
+  // }
+  // return beams.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log('the answer to Part Two may be', total);
