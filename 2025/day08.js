ba2f34deb10068f93774d70cbc67399f24e68ada
TomGower <tom.gower@gmail.com>
2025-12-10 13:24:04 -0600

    2025 Day 8 Part 2

--- 2025/day08.js
+++ 2025/day08.js
@@ -65,3 +65,54 @@ function partOne(maxSize = 1000) {
 }
 
 console.log('the answer to Part One may be', partOne());
+
+class UnionFind {
+  constructor(n) {
+    this.parent = new Array(n).fill().map((_, i) => i);
+    this.rank = new Array(n).fill(0);
+    this.components = n;
+  }
+
+  find(x) {
+    if (this.parent[x] !== x) {
+      this.parent[x] = this.find(this.parent[x]);
+    }
+    return this.parent[x];
+  }
+
+  union(x, y) {
+    const xgroup = this.find(x);
+    const ygroup = this.find(y);
+    if (xgroup === ygroup) return false;
+    if (this.rank[xgroup] > this.rank[ygroup]) {
+      this.parent[ygroup] = xgroup;
+    } else if (this.rank[xgroup] < this.rank[ygroup]) {
+      this.parent[xgroup] = ygroup;
+    } else {
+      this.parent[ygroup] = xgroup;
+      this.rank[xgroup]++;
+    }
+    this.components = this.components - 1;
+    return this.components === 1;
+  }
+}
+
+function partTwo() {
+  const distances = [];
+  const n = values.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const dist = getDistance(i, j);
+      distances.push([dist, i, j]);
+    }
+  }
+  distances.sort((a, b) => a[0] - b[0]);
+  const uf = new UnionFind(n);
+  for (const [_dist, i, j] of distances) {
+    const isDone = uf.union(i, j);
+    if (isDone) return values[i][0] * values[j][0];
+  }
+  return -1;
+}
+
+console.log('the answer to Part Two may be', partTwo());
